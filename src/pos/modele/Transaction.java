9f86956c6f6e35f4371766565fb9f419e52fbd67
1863855 <1863855@LQ3505-16.cegeplimoilou.lan>
2020-02-13 14:35:55 -0500

    Début de la classe métier Transaction (" " = non fait, "-" = en cours et "+" = fait)
    + Variable + javadoc
    - nom méthode + javadoc
    " " tests
    " "  corps des méthodes

--- src/pos/modele/Transaction.java
+++ src/pos/modele/Transaction.java
@@ -1,5 +1,45 @@
 package pos.modele;
 
+/**
+ * Cette classe métier s'occupe des transactions
+ */
 public class Transaction {
+	/**
+	 * Heure à laquelle la transaction a été effectué
+	 */
+	private String heure;
+	/**
+	 * Liste des produits sélectionnés par l'utilisateur
+	 */
+	//private List<Produits>;
+	/**
+	 * Valeur du sous-total de la facture
+	 */
+	private float sousTotal;
+	/**
+	 * Pourcentage de taxe a appliqué
+	 */
+	private float pourcentageTaxes = 0.15f;
+	/**
+	 * Montant de taxe que nous devons rajouter à la facture
+	 */
+	private float montantTaxes;
+	/**
+	 * Montant total de la facture qui sera facturé au client
+	 */
+	private float montantTotal;
+	/**
+	 * Numéro de la transaction 
+	 */
+	private long numero;
+	
+	/**
+	 * 
+	 * @param heure
+	 * @param pourcentageTaxes
+	 */
+	public Transaction(/*Etablissement etablissement, */String heure, /*arrayList<Produits> produits*/ float pourcentageTaxes) {
+		// TODO Auto-generated constructor stub
+	}
 
 }
