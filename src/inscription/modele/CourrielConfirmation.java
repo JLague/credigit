24792ea07e66544c54582b23a883015615e6c371
1833970 <1833970@LQ3505-14.cegeplimoilou.lan>
2020-02-20 17:53:11 -0500

    Changements dans courriel (Thread). Pas fini.

--- src/inscription/modele/CourrielConfirmation.java
+++ src/inscription/modele/CourrielConfirmation.java
@@ -4,57 +4,73 @@ import java.util.*;
 import javax.mail.*;
 import javax.mail.internet.*;
 
+import javafx.application.Platform;
+import javafx.concurrent.Task;
+
 public class CourrielConfirmation {
-	
-	static public boolean envoyerCourriel(String adresse, String Prenom)
-    {    
-        
+
+	private static final String USER = "credigit.bankera@gmail.com";
+	private static final String PASSWORD = "78f6ENKUgp2HHtr33Da4q";
+
+	private Session session;
+
+	public CourrielConfirmation() {
+		Task<Void> connexion = new Task<Void>() {
+
+			@Override
+			protected Void call() throws Exception {
+				Platform.runLater(() -> {
+					Properties properties = System.getProperties();
+					properties.put("mail.smtp.host", "smtp.gmail.com");
+					properties.put("mail.smtp.port", "587");
+					properties.put("mail.smtp.auth", "true");
+					properties.put("mail.smtp.starttls.enable", "true");
+
+					session = Session.getDefaultInstance(properties, new javax.mail.Authenticator() {
+						protected PasswordAuthentication getPasswordAuthentication() {
+							return new PasswordAuthentication(USER, PASSWORD);
+						}
+					});
+				});
+
+				return null;
+			}
+
+		};
+
+		new Thread(connexion).start();
+	}
+
+	public boolean envoyerCourriel(String adresse, String Prenom) {
+
 		boolean envoye = true;
-		
-		String to= adresse;   
-        final String user="credigit.bankera@gmail.com";   
-        final String password="78f6ENKUgp2HHtr33Da4q";   
 
-        //1) Ouvre la session pour envoyer un courriel   
-        Properties properties = System.getProperties();  
-        properties.put("mail.smtp.host", "smtp.gmail.com");  
-        properties.put("mail.smtp.port", "587"); 
-        properties.put("mail.smtp.auth", "true");   
-        properties.put("mail.smtp.starttls.enable", "true"); 
+		String to = adresse;
+		try {
+			MimeMessage message = new MimeMessage(session);
+			message.setFrom(new InternetAddress(USER));
+			message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+			message.setSubject("Bienvenue dans la famille!");
 
-        Session session = Session.getDefaultInstance(properties,   
-                new javax.mail.Authenticator() {   
-            protected PasswordAuthentication getPasswordAuthentication() {   
-                return new PasswordAuthentication(user,password);    }   });       
+			// 3) Crée le corps du message
+			BodyPart messageBodyPart1 = new MimeBodyPart();
+			messageBodyPart1.setText("Bonjour " + Prenom + ",\n\nLa grande famille de Credigit est heureuse de vous"
+					+ " compter comme un nouveau membre! Avec Credigit, vous rendez vos transactions bancaires plus sécuritaires.\n\n"
+					+ "Si vous rencontrez des difficultés, n'hésitez pas à contacter notre service à la clientèle au 418-803-5698.\n\n"
+					+ "Continuez à avoir l'argent au bout de votre doigt,\n\n" + "L'équipe de Crédigit\n"
+					+ "Une filiale de Bank-era Corp");
 
-        //2) Composition du message      
-        try{    
-            MimeMessage message = new MimeMessage(session);    
-            message.setFrom(new InternetAddress(user));     
-            message.addRecipient(Message.RecipientType.TO,new InternetAddress(to));    
-            message.setSubject("Bienvenue dans la famille!");         
+			// 4) Set le contenu du message
+			message.setContent(new MimeMultipart(messageBodyPart1));
 
-            //3) Crée le corps du message        
-            BodyPart messageBodyPart1 = new MimeBodyPart();     
-            messageBodyPart1.setText("Bonjour " + Prenom + ",\n\nLa grande famille de Credigit est heureuse de vous"
-            		+ " compter comme un nouveau membre! Avec Credigit, vous rendez vos transactions bancaires plus sécuritaires.\n\n"
-            		+ "Si vous rencontrez des difficultés, n'hésitez pas à contacter notre service à la clientèle au 418-803-5698.\n\n"
-            		+ "Continuez à avoir l'argent au bout de votre doigt,\n\n"
-            		+ "L'équipe de Crédigit\n"
-            		+ "Une filiale de Bank-era Corp");                     
-  
-            //4) Set le contenu du message   
-            message.setContent(new MimeMultipart(messageBodyPart1));        
+			// 5) Envoir le message
+			Transport.send(message);
 
-            //5) Envoir le message 
-            Transport.send(message);       
+		} catch (MessagingException ex) {
+			envoye = false;
+		}
 
-        }catch (MessagingException ex) 
-        {
-        	envoye = false;
-        }  
-        
-        return envoye;
-    }
+		return envoye;
+	}
 
 }
