b3c3bb495ca6f7b9eff10182a765b63d2f22ba38
1833970 <1833970@LQ3505-14.cegeplimoilou.lan>
2020-02-20 17:21:50 -0500

    On peut ajouter des clients!

--- src/inscription/controleur/InscriptionCtrl.java
+++ src/inscription/controleur/InscriptionCtrl.java
@@ -1,14 +1,9 @@
 package inscription.controleur;
 
-import java.time.LocalDate;
-import java.util.ArrayList;
-
 import inscription.modele.Client;
 import inscription.modele.Connexion;
 import inscription.modele.DataTransition;
 import inscription.modele.ExceptionCreationCompte;
-import inscription.modele.LocalAdresse;
-import inscription.modele.Questions;
 import inscription.vue.InscriptionVueCtrl;
 import javafx.scene.Scene;
 
@@ -26,24 +21,7 @@ public class InscriptionCtrl {
 		return vue.getScene();
 	}
 
-	public void envoyerDataClient(DataTransition data) {
-		Client client = null;
-		try {
-			LocalDate localDate = LocalDate.of(2001, 4, 24);
-			ArrayList<Questions> questions = new ArrayList<Questions>();
-			ArrayList<String> reponses = new ArrayList<String>();
-			questions.add(Questions.ANIMAL);
-			questions.add(Questions.ECOLE);
-			reponses.add("Chien");
-			reponses.add("St-Pierre");
-			client = new Client(new DataTransition("Duchesne", "Christophe", "youremail@here.com", localDate,
-					new LocalAdresse("555 rue Yolo", 12, "G5C 4F7", "Québec", "Québec", "Canada"), "111111111",
-					questions, reponses, "5555555555", null));
-			connexion.ajouterCompte(client);
-		} catch (ExceptionCreationCompte e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
+	public void envoyerDataClient(DataTransition data) throws ExceptionCreationCompte {
+		connexion.ajouterCompte(new Client(data));
 	}
 }
