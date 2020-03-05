1577105d92da79839b1a30aae5c3fc936ee5af47
1835287 <1835287@lq3516-13.cegeplimoilou.lan>
2020-03-05 09:26:40 -0500

    Les constructeurs de produit et d'établissement sont différents.
    Veuillez ajuster votre code en conséquence...

--- src/pos/modele/Etablissement.java
+++ src/pos/modele/Etablissement.java
@@ -3,6 +3,11 @@ package pos.modele;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Classe créant un établissement
+ * @author Bank-era Corp.
+ *
+ */
 public class Etablissement {
 
 	/**
@@ -25,83 +30,158 @@ public class Etablissement {
 	 * Balance de la compagnie
 	 */
 	private float balance = 0;
+	/**
+	 * Liste contenant les transactions d'un établissement
+	 */
+	private List<Transaction> transactions;
 
-	public Etablissement(String nom, String adresse, List<Produit> inventaire, List<Transaction> transactions,
-			float balance) {
-	}
-
-	public Etablissement(String nom, String adresse) {
-		this(nom, adresse, new ArrayList<Produit>(), new ArrayList<Transaction>(), 0f);
+	/**
+	 * Crée un établissement
+	 * @param nom - Nom de l'établissement
+	 * @param adresse - Adresse de l'établissement
+	 * @param balance - Balance de l'établissement
+	 */
+	public Etablissement(String nom, String adresse, float balance) {
+		
+		setNom(nom);
+		setAdresse(adresse);
+		setBalance(balance);
+		
+		inventaire = new ArrayList<Produit>();
+		transactions = new ArrayList<Transaction>();
+		utilisateurs = new ArrayList<Utilisateur>();
 	}
 
 	/**
-	 * @return le nom
+	 * Retourne le nom de l'établissement
+	 * @return Le nom de l'établissement
 	 */
 	public String getNom() {
 		return nom;
 	}
 
 	/**
-	 * @param nom le nom à modifier
+	 * Modifie le nom de l'établissement
+	 * @param nom - Le nom de l'établissement à modifier
 	 */
-	public void setNom(String nom) {
-		this.nom = nom;
+	protected void setNom(String nom) {
+		
+		if(nom != null && nom.length() != 0)
+		{
+			this.nom = nom;
+		}
+		
 	}
 
 	/**
-	 * @return le adresse
+	 * Retourne l'adresse de l'établissement
+	 * @return L'adresse de l'établissement
 	 */
 	public String getAdresse() {
 		return adresse;
 	}
 
 	/**
-	 * @param adresse le adresse à modifier
+	 * Modifie l'adresse de l'établissement
+	 * @param adresse - L'adresse de l'établissement à modifier
 	 */
-	public void setAdresse(String adresse) {
-		this.adresse = adresse;
+	protected void setAdresse(String adresse) {
+		
+		if(adresse != null && adresse.length() != 0)
+		{
+			this.adresse = adresse;
+		}
+		
 	}
 
 	/**
-	 * @return le inventaire
+	 * Retourne l'inventaire de l'établissement
+	 * @return L'inventaire de l'établissement
 	 */
 	public List<Produit> getInventaire() {
 		return inventaire;
 	}
 
 	/**
-	 * @param inventaire le inventaire à modifier
+	 * Ajoute un produit à l'inventaire de l'établissement
+	 * @param produit - Le produit à ajouter
 	 */
-	public void setInventaire(List<Produit> inventaire) {
-		this.inventaire = inventaire;
+	protected void ajouterProduitInventaire(Produit produit) {
+		
+		if(produit != null)
+		{
+			inventaire.add(produit);
+		}	
 	}
 
 	/**
-	 * @return le utilisateurs
+	 * Retourne les utilisateurs de l'établissement
+	 * @return Les utilisateurs de de l'établissement
 	 */
 	public List<Utilisateur> getUtilisateurs() {
 		return utilisateurs;
 	}
 
 	/**
-	 * @param utilisateurs le utilisateurs à modifier
+	 * Ajoute un utilisateur à l'établissement
+	 * @param utilisateur - L'utilisateur à ajouter
 	 */
-	public void setUtilisateurs(List<Utilisateur> utilisateurs) {
-		this.utilisateurs = utilisateurs;
+	protected void ajouterUtilisateur(Utilisateur utilisateur) {
+		
+		if(utilisateur != null)
+		{
+			utilisateurs.add(utilisateur);
+		}
+		
 	}
 
 	/**
-	 * @return le balance
+	 * Retourne la balance de l'établissement
+	 * @return La balance de l'établissement
 	 */
 	public float getBalance() {
 		return balance;
 	}
 
 	/**
-	 * @param balance le balance à modifier
+	 * Modifie la balance de l'établissement
+	 * @param balance - La balance de l'établissement à modifier
 	 */
-	public void setBalance(float balance) {
+	protected void setBalance(float balance) {
 		this.balance = balance;
 	}
+	
+	/**
+	 * Ajoute le montant de l'ajout à la balance
+	 * 
+	 * @param ajout - Le montant à ajouter
+	 */
+	protected void ajouterBalance(float ajout)
+	{
+		balance += ajout;
+	}
+	
+	/**
+	 * Retourne les transcations de l'établissement
+	 * @return Les transactions de l'établissement
+	 */
+	public List<Transaction> getTransactions() {
+		return transactions;
+	}
+
+	/**
+	 * Ajoute une transactions à la liste de transactions de l'établissement
+	 * @param transaction - La transaction à ajouter
+	 */
+	public void ajouterTransaction(Transaction transaction) {
+		
+		if(transaction != null)
+		{
+			transactions.add(transaction);
+		}
+		
+	}
+	
+	
 
 }
--- src/pos/modele/Produit.java
+++ src/pos/modele/Produit.java
@@ -1,5 +1,14 @@
 package pos.modele;
 
+
+import java.io.ByteArrayOutputStream;
+
+/**
+ * Classe créant un produit
+ * 
+ * @author Bank-era Corp.
+ *
+ */
 public class Produit {
 
 	/**
@@ -36,112 +45,200 @@ public class Produit {
 	 * La quantité de ce produit
 	 */
 	private int quantite;
+	
+	/**
+	 * L'image du produit
+	 */
+	private ByteArrayOutputStream image;
 
-	public Produit(long sku, String nom, float prix, float coutant, String fournisseur, String description) {
-		this.sku = sku;
-		this.nom = nom;
-		this.prix = prix;
-		this.coutant = coutant;
-		this.fournisseur = fournisseur;
-		this.description = description;
+	/**
+	 * Crée un nouveau produit
+	 * 
+	 * @param sku - Le stock keeping unit du produit
+	 * @param nom - Le nom du produit
+	 * @param prix - Le prix du produit
+	 * @param coutant - Le prix coutant du produit
+	 * @param fournisseur - Le fournisseur du produit
+	 * @param quantite - La quantité du produit
+	 * @param description - La description du produit
+	 * @param image - L'image du produit
+	 */
+	public Produit(long sku, String nom, float prix, float coutant, String fournisseur, int quantite, String description, ByteArrayOutputStream image) {
+		setSku(sku);
+		setNom(nom);
+		setPrix(prix);
+		setCoutant(coutant);
+		setFournisseur(fournisseur);
+		setQuantite(quantite);
+		setDescription(description);
+		setImage(image);
 	}
 
 	/**
-	 * @return the sku
+	 * Retourne le sku du produit
+	 * 
+	 * @return Le sku du produit
 	 */
 	public long getSku() {
 		return sku;
 	}
 
 	/**
-	 * @param sku the sku to set
+	 * Modifie le sku du produit
+	 * 
+	 * @param sku - Le sku à modifier
 	 */
-	public void setSku(long sku) {
-		this.sku = sku;
+	protected void setSku(long sku) {
+		if(sku > 0)
+		{
+			this.sku = sku;
+		}
+		
 	}
 
 	/**
-	 * @return the nom
+	 * Retourne le nom du produit
+	 * 
+	 * @return Le nom du produit
 	 */
 	public String getNom() {
 		return nom;
 	}
 
 	/**
-	 * @param nom the nom to set
+	 * Modifie le nom du produit
+	 * 
+	 * @param nom - Le nom à modifier
 	 */
-	public void setNom(String nom) {
-		this.nom = nom;
+	protected void setNom(String nom) {
+		if(nom != null && nom.length() != 0)
+		{
+			this.nom = nom;
+		}
+		
 	}
 
 	/**
-	 * @return the prix
+	 * Retourne le prix du produit
+	 * 
+	 * @return Le prix du produit
 	 */
 	public float getPrix() {
 		return prix;
 	}
 
 	/**
-	 * @param prix the prix to set
+	 * Modifie le prix du produit
+	 * 
+	 * @param prix - Le prix du produit à modifier
 	 */
-	public void setPrix(float prix) {
-		this.prix = prix;
+	protected void setPrix(float prix) {
+		if(prix >= 0)
+		{
+			this.prix = prix;
+		}
+		
 	}
 
 	/**
-	 * @return the coutant
+	 * Retourne le prix coutant du produit
+	 * 
+	 * @return Le prix coutant du produit
 	 */
 	public float getCoutant() {
 		return coutant;
 	}
 
 	/**
-	 * @param coutant the coutant to set
+	 * Modifie le prix coutant du produit
+	 * 
+	 * @param coutant - Le prix coutant du produit à modifier
 	 */
-	public void setCoutant(float coutant) {
-		this.coutant = coutant;
+	protected void setCoutant(float coutant) {
+		
+		if(coutant >= 0)
+		{
+			this.coutant = coutant;
+		}
 	}
 
 	/**
-	 * @return the fournisseur
+	 * Retourne le fournisseur du produit
+	 * 
+	 * @return Le fournisseur du produit
 	 */
 	public String getFournisseur() {
 		return fournisseur;
 	}
 
 	/**
-	 * @param fournisseur the fournisseur to set
+	 * Modifie  le fournisseur du produit
+	 * 
+	 * @param fournisseur - Le fournisseur du produit
 	 */
-	public void setFournisseur(String fournisseur) {
-		this.fournisseur = fournisseur;
+	protected void setFournisseur(String fournisseur) {
+		
+		if(fournisseur != null && fournisseur.length() != 0)
+		{
+			this.fournisseur = fournisseur;
+		}
+		
 	}
 
 	/**
-	 * @return the description
+	 * Retourne la description du produit
+	 * 
+	 * @return Le description du produit
 	 */
 	public String getDescription() {
 		return description;
 	}
 
 	/**
-	 * @param description the description to set
+	 * Modifie la description du produit
+	 * @param description - La description du produit à modifier
 	 */
-	public void setDescription(String description) {
+	protected void setDescription(String description) {
 		this.description = description;
 	}
 
 	/**
-	 * @return the quantite
+	 * Retourne la quantite du produit
+	 * @return La quantite du produit
 	 */
 	public int getQuantite() {
 		return quantite;
 	}
 
 	/**
-	 * @param quantite the quantite to set
+	 * Modifie la quantite du produit
+	 * @param quantite - La quantite du produit à modifier
 	 */
-	public void setQuantite(int quantite) {
-		this.quantite = quantite;
+	protected void setQuantite(int quantite) {
+		if(quantite >= 0)
+		{
+			this.quantite = quantite;
+		}	
+	}
+	
+	/**
+	 * Retourne l'objet contenant l'image
+	 * 
+	 * @return L'objet contenant l'image
+	 */
+	public ByteArrayOutputStream getImage() {
+		return image;
+	}
+
+	/**
+	 * Modifie l'objet contenant l'image
+	 * @param image - L'objet contenant l'image à modifier
+	 */
+	protected void setImage(ByteArrayOutputStream image) {
+		if(image != null && image.size() != 0)
+		{
+			this.image = image;
+		}
 	}
 
 	@Override
