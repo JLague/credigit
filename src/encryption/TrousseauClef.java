647c0137c79a77d5d40ac7481967a74230a7c323
DystopianSalmonWithTwoKnives <DystopianSalmonWithTwoKnives>
2020-04-19 16:46:51 -0400

    Squelette, erreur IndexOutOfBound

--- src/encryption/TrousseauClef.java
+++ src/encryption/TrousseauClef.java
@@ -1,19 +1,11 @@
 package encryption;
 
-import java.util.HashMap;
-import java.util.Map;
-
 /**
  * Cette classe s'occupe de garder et générer les clés utilisé par l'algorithme.
  */
 public class TrousseauClef {
 	int[][] cléActuelle;
 	int round;
-
-	int[] w1;
-	int[] w2;
-	int[] w3;
-	int[] w4;
 
 	public TrousseauClef(boolean encrypte, int[][] clé) {
 		if (encrypte) {
@@ -22,22 +14,17 @@ public class TrousseauClef {
 			round = 1;
 		}
 
-		w1 = new int[4];
-		w2 = new int[4];
-		w3 = new int[4];
-		w4 = new int[4];
-
 		cléActuelle = new int[4][4];
 		this.cléActuelle = clé;
 	}
 
 	public int[][] getProchaine() {
 
-		for (int i = 0; i < 4; i++) {
-			w1[i] = cléActuelle[i][0];
-			w2[i] = cléActuelle[i][1];
-			w3[i] = cléActuelle[i][2];
-			w4[i] = cléActuelle[i][3];
+		for (int i = 0; i < cléActuelle[0].length; i++) {
+			cléActuelle[i][0] = cléActuelle[i][0] + fonctionG(cléActuelle[0])[i];
+			cléActuelle[i][1] = Math.floorMod(cléActuelle[i][0] ^ cléActuelle[i][1], 256);
+			cléActuelle[i][2] = Math.floorMod(cléActuelle[i][1] ^ cléActuelle[i][2], 256);
+			cléActuelle[i][3] = Math.floorMod(cléActuelle[i][2] ^ cléActuelle[i][3], 256);
 		}
 
 		round++;
@@ -50,7 +37,20 @@ public class TrousseauClef {
 		return cléActuelle;
 	}
 
-	private int substitutionAvant(int k) {
+	private int[] fonctionG(int[] w) {
+		int[] mot = { w[1], w[2], w[3], w[0] };
+
+		for (int i = 0; i < w.length; i++) {
+			mot[i] = subsAvant(mot[i]);
+		}
+
+		mot[0] = mot[0] ^ round;
+
+		return mot;
+	}
+
+	// Volé fonction temporaire // TODO remplacer
+	private static int subsAvant(int k) {
 
 		int[] t = new int[256];
 		for (int i = 0, x = 1; i < 256; i++) {
@@ -65,10 +65,11 @@ public class TrousseauClef {
 			x ^= (x >> 4) ^ (x >> 5) ^ (x >> 6) ^ (x >> 7);
 			S[t[i]] = (x ^ 0x63) & 0xFF;
 		}
-
 		return S[k];
 	}
-	private int substitutionArriere(int k) {
+
+	// Volé fonction temporaire // TODO remplacer
+	private static int subsArriere(int k) {
 
 		int[] t = new int[256];
 		for (int i = 0, x = 1; i < 256; i++) {
@@ -86,11 +87,16 @@ public class TrousseauClef {
 
 		return t[k];
 	}
-	
-	public static void main(String[] args) {
-		TrousseauClef t1 = new TrousseauClef(true, null);
-		System.out.println(t1.substitutionAvant(0x59));
-		System.out.println(t1.substitutionArriere(0xCB));
-	}
 
+	public static void main(String[] args) {
+		int[][] clé = { { 0x00, 0x01, 0x02, 0x03 }, { 0x04, 0x05, 0x06, 0x07 }, { 0x08, 0x09, 0x0a, 0x0b },
+				{ 0x0c, 0x0d, 0x0e, 0x0f } };
+		TrousseauClef t1 = new TrousseauClef(false, clé);
+
+		
+			System.out.println(MatriceUtilitaires.toStringMat(t1.getProchaine()));
+			System.out.println(MatriceUtilitaires.toStringMat(t1.getPrecedente()));
+			System.out.println(MatriceUtilitaires.toStringMat(t1.getPrecedente()));
+
+	}
 }
