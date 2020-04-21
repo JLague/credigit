69b75d2b7b329b2863828379f602aef3300c09f9
DystopianSalmonWithTwoKnives <DystopianSalmonWithTwoKnives>
2020-04-21 17:24:12 -0400

    Trousseau de clé fonction complet pour l'encryption

--- src/encryption/TrousseauClef.java
+++ src/encryption/TrousseauClef.java
@@ -6,99 +6,70 @@ package encryption;
  * @author Bank-era Corp.
  */
 public class TrousseauClef {
-	int[][] cléActuelle;
+	String clef;
 	int round;
 
-	public TrousseauClef(boolean encrypte, int[][] clé) {
-		if (encrypte) {
-			round = 10;
-		} else {
-			round = 1;
-		}
+	String mot0;
+	String mot1;
+	String mot2;
+	String mot3;
 
-		cléActuelle = new int[4][4];
-		this.cléActuelle = clé;
+	String nMot0;
+	String nMot1;
+	String nMot2;
+	String nMot3;
+
+	public TrousseauClef(int pRound, String pClef) {
+		this.round = pRound;
+		this.clef = pClef;
 	}
 
-	public int[][] getProchaine() {
-
-		for (int i = 0; i < cléActuelle[0].length; i++) {
-			cléActuelle[i][0] = cléActuelle[i][0] + fonctionG(cléActuelle[0])[i];
-			cléActuelle[i][1] = Math.floorMod(cléActuelle[i][0] ^ cléActuelle[i][1], 256);
-			cléActuelle[i][2] = Math.floorMod(cléActuelle[i][1] ^ cléActuelle[i][2], 256);
-			cléActuelle[i][3] = Math.floorMod(cléActuelle[i][2] ^ cléActuelle[i][3], 256);
-		}
-
-		round++;
-		return cléActuelle;
-	}
-
-	public int[][] getPrecedente() {
-
-		round--;
-		return cléActuelle;
-	}
-
-	private int[] fonctionG(int[] w) {
-		int[] mot = { w[1], w[2], w[3], w[0] };
-
-		for (int i = 0; i < w.length; i++) {
-			mot[i] = subsAvant(mot[i]);
-		}
-
-		mot[0] = mot[0] ^ round;
-
-		return mot;
-	}
-
-	// Volé fonction temporaire // TODO remplacer
-	private static int subsAvant(int k) {
-
-		int[] t = new int[256];
-		for (int i = 0, x = 1; i < 256; i++) {
-			t[i] = x;
-			x ^= (x << 1) ^ ((x >>> 7) * 0x11B);
-		}
-		int[] S = new int[256];
-		S[0] = 0x63;
-		for (int i = 0; i < 255; i++) {
-			int x = t[255 - i];
-			x |= x << 8;
-			x ^= (x >> 4) ^ (x >> 5) ^ (x >> 6) ^ (x >> 7);
-			S[t[i]] = (x ^ 0x63) & 0xFF;
-		}
-		return S[k];
-	}
-
-	// Volé fonction temporaire // TODO remplacer
-	private static int subsArriere(int k) {
-
-		int[] t = new int[256];
-		for (int i = 0, x = 1; i < 256; i++) {
-			t[i] = x;
-			x ^= (x << 1) ^ ((x >>> 7) * 0x11B);
-		}
-		int[] S = new int[256];
-		S[0] = 0x63;
-		for (int i = 0; i < 255; i++) {
-			int x = t[255 - i];
-			x |= x << 8;
-			x ^= (x >> 4) ^ (x >> 5) ^ (x >> 6) ^ (x >> 7);
-			S[t[i]] = (x ^ 0x63) & 0xFF;
-		}
-
-		return t[k];
-	}
-
-	public static void main(String[] args) {
-		int[][] clé = { { 0x00, 0x01, 0x02, 0x03 }, { 0x04, 0x05, 0x06, 0x07 }, { 0x08, 0x09, 0x0a, 0x0b },
-				{ 0x0c, 0x0d, 0x0e, 0x0f } };
-		TrousseauClef t1 = new TrousseauClef(false, clé);
-
+	/**
+	 * Génère la prochaine clée sous forme de String
+	 */
+	public String getProchaine() {
+		mot0 = clef.substring(0,4);
+		mot1 = clef.substring(4,8);
+		mot2 = clef.substring(8,12);
+		mot3 = clef.substring(12,16);
 		
-			System.out.println(MatriceUtilitaires.toStringMat(t1.getProchaine()));
-			System.out.println(MatriceUtilitaires.toStringMat(t1.getPrecedente()));
-			System.out.println(MatriceUtilitaires.toStringMat(t1.getPrecedente()));
+		nMot0 = "" + (Integer.parseInt("0x" + mot0)^Integer.parseInt("0x" + fonctionGAvant(mot3)));
+		nMot1 = "" + (Integer.parseInt("0x" + mot0)^Integer.parseInt("0x" + mot1));
+		nMot2 = "" + (Integer.parseInt("0x" + mot1)^Integer.parseInt("0x" + mot2));
+		nMot3 = "" + (Integer.parseInt("0x" + mot2)^Integer.parseInt("0x" + mot3));
+		
+		return nMot0+nMot1+nMot2+nMot3;
+	}
 
+	/**
+	 * Génère la clée précédente sous forme de string
+	 */
+	public String getPrecedente() {
+		return null;
+	}
+
+	/**
+	 * Accomplit la fonction g sur un mot
+	 * @param mot - Mot qui subira la fonction g
+	 * @return le mot modifié
+	 */
+	private String fonctionGAvant(String mot) {
+		String temp = mot.substring(1, 4) + mot.charAt(0);
+		temp = Encryption.substitutionAvant(temp.charAt(0)) + Encryption.substitutionAvant(temp.charAt(1))
+				+ Encryption.substitutionAvant(temp.charAt(2)) + Encryption.substitutionAvant(temp.charAt(3));
+		
+		
+		temp = (Integer.parseInt("0x" + temp.substring(0,1))^round) + temp.substring(1,4);
+		
+		return temp;
+	}
+
+	/**
+	 * Accomplit l'inverse de ka fonction g sur un mot
+	 * @param mot - Mot qui subira l'inverse de la fonction g
+	 * @return le mot modifié
+	 */
+	private String fonctionGArriere(String mot) {
+		return null;
 	}
 }
