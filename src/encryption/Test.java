bc5186fe61a96f285e5b314c2f328cdae7bab061
billy <billy@billy-manjaro>
2020-04-22 22:03:45 -0400

    Fixed bitwiseLongDivision

--- src/encryption/Test.java
+++ src/encryption/Test.java
@@ -15,27 +15,52 @@ public class Test {
 	}
 
 	public static void bitwiseLongDivision(int a, int d) {
-		d <<= (Integer.toBinaryString(a).length() - Integer.toBinaryString(d).length());
+//		d <<= (Integer.toBinaryString(a).length() - Integer.toBinaryString(d).length());
+//
+//		boolean flag = true;
+//
+//		if (d < a && d != 0) {
+//
+//			while (flag) {
+//				if ((Integer.toBinaryString(a).length() - Integer.toBinaryString(d).length()) >= 0) {
+//					if (a >= d) {
+//						int temp = Integer.toBinaryString(a).length();
+//
+//						a ^= d;
+//						d >>= temp - Integer.toBinaryString(a).length();
+//					} else
+//						flag = false;
+//				} else
+//					flag = false;
+//			}
+//
+//		} else if (d == a)
+//			a = 0;
 
-		boolean flag = true;
+		// Get leftmost bit of a and d
+		int lmba = 0, lmbd = 0;
+		while ((a >> lmba++) != 0);
+		while ((d >> lmbd++) != 0);
 
-		if (d < a && d != 0) {
+		// Met les variables à leur valeurs réelles
+		lmba -= 2;
+		lmbd -= 2;
+		
+		int shift = lmba - lmbd;
 
-			while (flag) {
-				if ((Integer.toBinaryString(a).length() - Integer.toBinaryString(d).length()) >= 0) {
-					if (a >= d) {
-						int temp = Integer.toBinaryString(a).length();
-
-						a ^= d;
-						d >>= temp - Integer.toBinaryString(a).length();
-					} else
-						flag = false;
-				} else
-					flag = false;
+		int lmb;
+		while (lmba != (lmbd - 1)) {
+			
+			// Retrieve leftmost bit
+			lmb = (1 << lmba--) & a;
+			
+			// Effectue une étape de la division
+			if (lmb != 0) {
+				a ^= (d << shift);
 			}
 
-		} else if (d == a)
-			a = 0;
+			shift--;
+		}
 
 		System.out.println(d + "  :  " + a);
 	}
@@ -43,9 +68,12 @@ public class Test {
 	public static void main(String[] args) {
 
 		bitwiseMultiply(84, 13);
-
-		bitwiseLongDivision(16254, 283);
+		bitwiseMultiply(0x95, 0x8A);
 		
+
+		bitwiseLongDivision(932, 0b101100011);
+		bitwiseLongDivision(16254, 283);
+		bitwiseLongDivision(20226, 283);
 
 	}
 
