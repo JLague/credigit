45cf62f4a52c371a7f70fedff7e4edf3526a48f7
DystopianSalmonWithTwoKnives <DystopianSalmonWithTwoKnives>
2020-04-29 21:19:50 -0400

    MixCol (encryption) est ok, y manque juste division polynomiale longue

--- src/encryption/AES.java
+++ src/encryption/AES.java
@@ -10,46 +10,44 @@ import java.util.*;
  */
 public class AES {
 
-	
 	/**
 	 * Tableau temporaire de substitution pour l'encrytion
 	 */
-	private static final String[] SUBSTITUTION_EN = { "63", "7C", "77", "7B", "F2", "6B", "6F", "C5", "30", "01", "67", "2B", "FE", "D7", "AB", "76",
-			"CA", "82", "C9", "7D", "FA", "59", "47", "F0", "AD", "D4", "A2", "AF", "9C", "A4", "72", "C0", "B7",
-			"FD", "93", "26", "36", "3F", "F7", "CC", "34", "A5", "E5", "F1", "71", "D8", "31", "15", "04", "C7",
-			"23", "C3", "18", "96", "05", "9A", "07", "12", "80", "E2", "EB", "27", "B2", "75", "09", "83", "2C",
-			"1A", "1B", "6E", "5A", "A0", "52", "3B", "D6", "B3", "29", "E3", "2F", "84", "53", "D1", "00", "ED",
-			"20", "FC", "B1", "5B", "6A", "CB", "BE", "39", "4A", "4C", "58", "CF", "D0", "EF", "AA", "FB", "43",
-			"4D", "33", "85", "45", "F9", "02", "7F", "50", "3C", "9F", "A8", "51", "A3", "40", "8F", "92", "9D",
-			"38", "F5", "BC", "B6", "DA", "21", "10", "FF", "F3", "D2", "CD", "0C", "13", "EC", "5F", "97", "44",
-			"17", "C4", "A7", "7E", "3D", "64", "5D", "19", "73", "60", "81", "4F", "DC", "22", "2A", "90", "88",
-			"46", "EE", "B8", "14", "DE", "5E", "0B", "DB", "E0", "32", "3A", "0A", "49", "06", "24", "5C", "C2",
-			"D3", "AC", "62", "91", "95", "E4", "79", "E7", "C8", "37", "6D", "8D", "D5", "4E", "A9", "6C", "56",
-			"F4", "EA", "65", "7A", "AE", "08", "BA", "78", "25", "2E", "1C", "A6", "B4", "C6", "E8", "DD", "74",
-			"1F", "4B", "BD", "8B", "8A", "70", "3E", "B5", "66", "48", "03", "F6", "0E", "61", "35", "57", "B9",
-			"86", "C1", "1D", "9E", "E1", "F8", "98", "11", "69", "D9", "8E", "94", "9B", "1E", "87", "E9", "CE",
-			"55", "28", "DF", "8C", "A1", "89", "0D", "BF", "E6", "42", "68", "41", "99", "2D", "0F", "B0", "54",
-			"BB", "16" };
-	
+	private static final String[] SUBSTITUTION_EN = { "63", "7C", "77", "7B", "F2", "6B", "6F", "C5", "30", "01", "67",
+			"2B", "FE", "D7", "AB", "76", "CA", "82", "C9", "7D", "FA", "59", "47", "F0", "AD", "D4", "A2", "AF", "9C",
+			"A4", "72", "C0", "B7", "FD", "93", "26", "36", "3F", "F7", "CC", "34", "A5", "E5", "F1", "71", "D8", "31",
+			"15", "04", "C7", "23", "C3", "18", "96", "05", "9A", "07", "12", "80", "E2", "EB", "27", "B2", "75", "09",
+			"83", "2C", "1A", "1B", "6E", "5A", "A0", "52", "3B", "D6", "B3", "29", "E3", "2F", "84", "53", "D1", "00",
+			"ED", "20", "FC", "B1", "5B", "6A", "CB", "BE", "39", "4A", "4C", "58", "CF", "D0", "EF", "AA", "FB", "43",
+			"4D", "33", "85", "45", "F9", "02", "7F", "50", "3C", "9F", "A8", "51", "A3", "40", "8F", "92", "9D", "38",
+			"F5", "BC", "B6", "DA", "21", "10", "FF", "F3", "D2", "CD", "0C", "13", "EC", "5F", "97", "44", "17", "C4",
+			"A7", "7E", "3D", "64", "5D", "19", "73", "60", "81", "4F", "DC", "22", "2A", "90", "88", "46", "EE", "B8",
+			"14", "DE", "5E", "0B", "DB", "E0", "32", "3A", "0A", "49", "06", "24", "5C", "C2", "D3", "AC", "62", "91",
+			"95", "E4", "79", "E7", "C8", "37", "6D", "8D", "D5", "4E", "A9", "6C", "56", "F4", "EA", "65", "7A", "AE",
+			"08", "BA", "78", "25", "2E", "1C", "A6", "B4", "C6", "E8", "DD", "74", "1F", "4B", "BD", "8B", "8A", "70",
+			"3E", "B5", "66", "48", "03", "F6", "0E", "61", "35", "57", "B9", "86", "C1", "1D", "9E", "E1", "F8", "98",
+			"11", "69", "D9", "8E", "94", "9B", "1E", "87", "E9", "CE", "55", "28", "DF", "8C", "A1", "89", "0D", "BF",
+			"E6", "42", "68", "41", "99", "2D", "0F", "B0", "54", "BB", "16" };
+
 	/**
 	 * Tableau temporaire de substitution pour la décryption
 	 */
-	private static final String[] SUBSTITUTION_DE = { "52", "09", "6A", "D5", "30", "36", "A5", "38", "BF", "40", "A3", "9E",
-			"81", "F3", "D7", "FB", "7C", "E3", "39", "82", "9B", "2F", "FF", "87", "34", "8E", "43", "44", "C4", "DE",
-			"E9", "CB", "54", "7B", "94", "32", "A6", "C2", "23", "3D", "EE", "4C", "95", "0B", "42", "FA", "C3", "4E",
-			"08", "2E", "A1", "66", "28", "D9", "24", "B2", "76", "5B", "A2", "49", "6D", "8B", "D1", "25", "72", "F8",
-			"F6", "64", "86", "68", "98", "16", "D4", "A4", "5C", "CC", "5D", "65", "B6", "92", "6C", "70", "48", "50",
-			"FD", "ED", "B9", "DA", "5E", "15", "46", "57", "A7", "8D", "9D", "84", "90", "D8", "AB", "00", "8C", "BC",
-			"D3", "0A", "F7", "E4", "58", "05", "B8", "B3", "45", "06", "D0", "2C", "1E", "8F", "CA", "3F", "0F", "02",
-			"C1", "AF", "BD", "03", "01", "13", "8A", "6B", "3A", "91", "11", "41", "4F", "67", "DC", "EA", "97", "F2",
-			"CF", "CE", "F0", "B4", "E6", "73", "96", "AC", "74", "22", "E7", "AD", "35", "85", "E2", "F9", "37", "E8",
-			"1C", "75", "DF", "6E", "47", "F1", "1A", "71", "1D", "29", "C5", "89", "6F", "B7", "62", "0E", "AA", "18",
-			"BE", "1B", "FC", "56", "3E", "4B", "C6", "D2", "79", "20", "9A", "DB", "C0", "FE", "78", "CD", "5A", "F4",
-			"1F", "DD", "A8", "33", "88", "07", "C7", "31", "B1", "12", "10", "59", "27", "80", "EC", "5F", "60", "51",
-			"7F", "A9", "19", "B5", "4A", "0D", "2D", "E5", "7A", "9F", "93", "C9", "9C", "EF", "A0", "E0", "3B", "4D",
-			"AE", "2A", "F5", "B0", "C8", "EB", "BB", "3C", "83", "53", "99", "61", "17", "2B", "04", "7E", "BA", "77",
-			"D6", "26", "E1", "69", "14", "63", "55", "21", "0C", "7D" };
-	
+	private static final String[] SUBSTITUTION_DE = { "52", "09", "6A", "D5", "30", "36", "A5", "38", "BF", "40", "A3",
+			"9E", "81", "F3", "D7", "FB", "7C", "E3", "39", "82", "9B", "2F", "FF", "87", "34", "8E", "43", "44", "C4",
+			"DE", "E9", "CB", "54", "7B", "94", "32", "A6", "C2", "23", "3D", "EE", "4C", "95", "0B", "42", "FA", "C3",
+			"4E", "08", "2E", "A1", "66", "28", "D9", "24", "B2", "76", "5B", "A2", "49", "6D", "8B", "D1", "25", "72",
+			"F8", "F6", "64", "86", "68", "98", "16", "D4", "A4", "5C", "CC", "5D", "65", "B6", "92", "6C", "70", "48",
+			"50", "FD", "ED", "B9", "DA", "5E", "15", "46", "57", "A7", "8D", "9D", "84", "90", "D8", "AB", "00", "8C",
+			"BC", "D3", "0A", "F7", "E4", "58", "05", "B8", "B3", "45", "06", "D0", "2C", "1E", "8F", "CA", "3F", "0F",
+			"02", "C1", "AF", "BD", "03", "01", "13", "8A", "6B", "3A", "91", "11", "41", "4F", "67", "DC", "EA", "97",
+			"F2", "CF", "CE", "F0", "B4", "E6", "73", "96", "AC", "74", "22", "E7", "AD", "35", "85", "E2", "F9", "37",
+			"E8", "1C", "75", "DF", "6E", "47", "F1", "1A", "71", "1D", "29", "C5", "89", "6F", "B7", "62", "0E", "AA",
+			"18", "BE", "1B", "FC", "56", "3E", "4B", "C6", "D2", "79", "20", "9A", "DB", "C0", "FE", "78", "CD", "5A",
+			"F4", "1F", "DD", "A8", "33", "88", "07", "C7", "31", "B1", "12", "10", "59", "27", "80", "EC", "5F", "60",
+			"51", "7F", "A9", "19", "B5", "4A", "0D", "2D", "E5", "7A", "9F", "93", "C9", "9C", "EF", "A0", "E0", "3B",
+			"4D", "AE", "2A", "F5", "B0", "C8", "EB", "BB", "3C", "83", "53", "99", "61", "17", "2B", "04", "7E", "BA",
+			"77", "D6", "26", "E1", "69", "14", "63", "55", "21", "0C", "7D" };
+
 	/**
 	 * Taille des matrices texte et des clés
 	 */
@@ -136,10 +134,9 @@ public class AES {
 			cpt++;
 		}
 	}
-	
-	private void remplirBoitesSubstitution()
-	{
-		//TODO À implémenter avec l'inverse multiplicatif
+
+	private void remplirBoitesSubstitution() {
+		// TODO À implémenter avec l'inverse multiplicatif
 	}
 
 	/**
@@ -202,54 +199,71 @@ public class AES {
 			}
 		}
 	}
-	
+
 	/**
 	 * Substitue chaque caractère de la liste de matrice de texte pour l'encryption
 	 */
-	protected void sBox()
-	{
-		for (String[][] matrice : texte) 
-			for (int i = 0; i < TAILLE; i++) 
-				for (int k = 0; k < TAILLE; k++) 
+	protected void sBox() {
+		for (String[][] matrice : texte)
+			for (int i = 0; i < TAILLE; i++)
+				for (int k = 0; k < TAILLE; k++)
 					matrice[i][k] = SUBSTITUTION_EN[Integer.parseInt(matrice[i][k], 16)];
 	}
-	
+
 	/**
 	 * Substitue chaque caractère de la liste de matrice de texte pour la décryption
 	 */
-	protected void sBoxInverse()
-	{
-		for (String[][] matrice : texte) 
-			for (int i = 0; i < TAILLE; i++) 
-				for (int k = 0; k < TAILLE; k++) 
+	protected void sBoxInverse() {
+		for (String[][] matrice : texte)
+			for (int i = 0; i < TAILLE; i++)
+				for (int k = 0; k < TAILLE; k++)
 					matrice[i][k] = SUBSTITUTION_DE[Integer.parseInt(matrice[i][k], 16)];
 	}
-	
+
 	/**
-	 * XOR la clé selon la round à chaque matrice de texte - Utilisez pour l'encryption et la décryption
+	 * XOR la clé selon la round à chaque matrice de texte - Utilisez pour
+	 * l'encryption et la décryption
+	 * 
 	 * @param round - La round de l'AES
 	 */
-	protected void addRoundKey(int round)
-	{
+	protected void addRoundKey(int round) {
 		String[][] cle = cles.getCle(round);
-		
-		for (String[][] matrice : texte) 
-			for (int i = 0; i < TAILLE; i++) 
-				for (int k = 0; k < TAILLE; k++) 
-					matrice[i][k] = Integer.toHexString(Integer.parseInt(matrice[i][k], 16) ^ 
-							Integer.parseInt(cle[i][k], 16));
-		
+
+		for (String[][] matrice : texte)
+			for (int i = 0; i < TAILLE; i++)
+				for (int k = 0; k < TAILLE; k++)
+					matrice[i][k] = Integer
+							.toHexString(Integer.parseInt(matrice[i][k], 16) ^ Integer.parseInt(cle[i][k], 16));
+
 	}
-	
-	protected void mixColumn()
-	{
-		//TODO à implémenter
+
+	protected String mixColumn(String m) {
+		String s = "";
+		for (int j = 0; j < 4; j++) {
+			for (int i = 0; i < 4; i++) {
+				s += Integer.toHexString(Integer.parseInt(getNByte(m, i * 4 + 0), 16) * 2
+						+ Integer.parseInt(getNByte(m, i * 4 + 5), 16) * 3
+						+ Integer.parseInt(getNByte(m, i * 4 + 10), 16)
+						+ Integer.parseInt(getNByte(m, i * 4 + 15), 16));
+				s += Integer.toHexString(
+						Integer.parseInt(getNByte(m, i * 4 + 0), 16) + Integer.parseInt(getNByte(m, i * 4 + 5), 16) * 2
+								+ Integer.parseInt(getNByte(m, i * 4 + 10), 16) * 3
+								+ Integer.parseInt(getNByte(m, i * 4 + 15), 16));
+				s += Integer.toHexString(
+						Integer.parseInt(getNByte(m, i * 4 + 0), 16) + Integer.parseInt(getNByte(m, i * 4 + 5), 16)
+								+ Integer.parseInt(getNByte(m, i * 4 + 10), 16) * 2
+								+ Integer.parseInt(getNByte(m, i * 4 + 15), 16) * 3);
+				s += Integer.toHexString(Integer.parseInt(getNByte(m, i * 4 + 0), 16) * 3
+						+ Integer.parseInt(getNByte(m, i * 4 + 5), 16) + Integer.parseInt(getNByte(m, i * 4 + 10), 16)
+						+ Integer.parseInt(getNByte(m, i * 4 + 15), 16) * 2);
+			}
+		}
+
+		return s;
 	}
-	
-	
-	protected void mixColumnInverse()
-	{
-		//TODO à implémenter
+
+	protected void mixColumnInverse() {
+		// TODO à implémenter
 	}
 
 	/**
@@ -260,8 +274,8 @@ public class AES {
 	 * @return le message codé
 	 */
 	public static String encrypter(String cle, String message) {
-		// TODO À implémenter
-		return null;
+		String messageCode = "";
+		return messageCode;
 	}
 
 	/**
@@ -275,16 +289,34 @@ public class AES {
 		// TODO À implémenter
 		return null;
 	}
-	
+
+	/**
+	 * Return le nieme byte de la string, considère la chaine comme un gallois field
+	 * 
+	 * @param s la chaine
+	 * @param n la position du byte voulu
+	 * @return le byte voulu
+	 */
+	private static String getNByte(String s, int n) {
+		int i = 0;
+
+		if (n * 2 > s.length())
+			i = n - (s.length() / 2);
+		else
+			i = n;
+
+		return s.substring(i * 2, i * 2 + 2);
+	}
+
 	/**
 	 * Cette classe s'occupe de garder et générer les clés utilisé par l'algorithme.
 	 * 
 	 * @author Bank-era Corp.
 	 */
 	private class TrousseauClef {
-		
+
 		private List<String[][]> cles;
-		
+
 		private String clef;
 		private int round;
 
@@ -304,45 +336,41 @@ public class AES {
 				this.clef = pClef.substring(0, 32);
 			else
 				this.clef = "00000000000000000000000000000000".substring(0, 32 - pClef.length()) + pClef;
-			
+
 			cles = new ArrayList<>();
 			round = 0;
-			
+
 			genererCles();
 		}
-		
-		protected String[][] getCle(int index)
-		{
+
+		protected String[][] getCle(int index) {
 			return cles.get(index);
 		}
-		
+
 		/**
 		 * Génére toute les matrices de clés nécessaires pour l'encryption
 		 */
-		private void genererCles()
-		{
+		private void genererCles() {
 			remplirMatrice();
-			while (round < 10)
-			{
+			while (round < 10) {
 				genererProchaine();
 			}
 		}
-		
+
 		/**
 		 * Remplis la matrice pour la clé et l'ajoute à la liste de clé
 		 */
-		private void remplirMatrice()
-		{
+		private void remplirMatrice() {
 			String[][] cle = new String[TAILLE][TAILLE];
-			
+
 			int cpt = 0;
-			
-			for (int j = 0; j < TAILLE; j++) 
+
+			for (int j = 0; j < TAILLE; j++)
 				for (int k = 0; k < TAILLE; k++) {
-					cle[k][j] = clef.substring(cpt, cpt+2);
+					cle[k][j] = clef.substring(cpt, cpt + 2);
 					cpt += 2;
-				}	
-			
+				}
+
 			cles.add(cle);
 		}
 
@@ -385,8 +413,7 @@ public class AES {
 		private String fonctionGAvant(String mot) {
 			String temp = mot.substring(2, 8) + mot.substring(0, 2);
 			temp = substitutionAvant(temp.substring(0, 2)) + substitutionAvant(temp.substring(2, 4))
-					+ substitutionAvant(temp.substring(4, 6))
-					+ substitutionAvant(temp.substring(6, 8));
+					+ substitutionAvant(temp.substring(4, 6)) + substitutionAvant(temp.substring(6, 8));
 
 			int rc = 1 << round;
 
@@ -401,7 +428,7 @@ public class AES {
 				temp = "0" + temp;
 			return temp;
 		}
-		
+
 		/**
 		 * Tableau de substitution de Rinjdael pour l'encryption
 		 * 
@@ -416,17 +443,17 @@ public class AES {
 
 		public String toString() {
 			String roundStyle;
-			
-			if(round < 10)
-				roundStyle = " "+ round;
+
+			if (round < 10)
+				roundStyle = " " + round;
 			else
 				roundStyle = round + "";
-			
-			return "[" + roundStyle+ "] k = " + clef;
+
+			return "[" + roundStyle + "] k = " + clef;
 		}
 
 	}
-	
+
 	/**
 	 * Méthode permettant de padder une String selon un bloc de 128-bit (16 bytes)
 	 * 
@@ -435,16 +462,16 @@ public class AES {
 	 */
 	private static String padString(String s) {
 		byte count = 0;
-		while(s.getBytes().length % 15 != 0) {
+		while (s.getBytes().length % 15 != 0) {
 			count++;
 			s += " ";
 		}
-		
+
 		s += (char) count;
-		
+
 		return s;
 	}
-	
+
 	/**
 	 * Méthode permettant de padder une String selon un bloc de 128-bit (16 bytes)
 	 * 
@@ -452,7 +479,12 @@ public class AES {
 	 * @return la String paddé
 	 */
 	private static String unpadString(String s) {
-		byte paddingLength = (byte) s.charAt(s.length()-1);
-		return s.substring(0, s.length()-paddingLength-1);
+		byte paddingLength = (byte) s.charAt(s.length() - 1);
+		return s.substring(0, s.length() - paddingLength - 1);
+	}
+
+	public static void main(String[] args) {
+		AES aes = new AES("000102030405060708090a0b0c0d0e0f", "00112233445566778899aabbccddeeff");
+		System.out.println(aes.mixColumn("6353e08c0960e104cd70b751bacad0e7"));
 	}
 }
